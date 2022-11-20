5651e59f2f543e97f97858ceec4eba46443ba240
Sortren <sortren.dev@gmail.com>
2022-11-20 23:58:00 +0100

    feat: buffer reading/writing

--- client/client.go
+++ client/client.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"github.com/Sortren/go-client-server-example/stream"
+	"log"
+	"net"
+)
+
 func main() {
+	conn, err := net.Dial("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("can't dial on localhost:8080, err: %s", err)
+	}
+
+	if err := stream.Write(conn, "[client] hello server!"); err != nil {
+		log.Fatalf("can't write to the stream, err: %s", err)
+	}
+
+	if err := stream.Read(conn); err != nil {
+		log.Fatalf("can't read from the stream, err: %s", err)
+	}
 
 }
